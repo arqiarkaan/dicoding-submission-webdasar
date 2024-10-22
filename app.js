22db0bc9f40a281c211319dd3ec7e13af9ea9473
arqiarkaan <darren.arqiarkaan.dyazfajri.teddy.tik22@mhsw.pnj.ac.id>
2024-10-22 11:10:24 +0700

    add middleware for static file

--- app.js
+++ app.js
@@ -2,10 +2,15 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
+// Middleware untuk file statis dari direktori root
+app.use(express.static(path.join(__dirname)));
+
+// Rute default untuk file index.html
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'index.html'));
 });
 
+// Janalin server pada port 3000
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
